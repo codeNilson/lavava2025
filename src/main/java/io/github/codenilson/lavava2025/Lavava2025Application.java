e534f983ac350a0a76dfaae07bed99eafa46e511
codenilson <fcodenilson@gmail.com>
2025-05-17 15:57:54 -0300

    first commit

---
+++ src/main/java/io/github/codenilson/lavava2025/Lavava2025Application.java
@@ -0,0 +1,13 @@
+package io.github.codenilson.lavava2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lavava2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lavava2025Application.class, args);
+	}
+
+}
