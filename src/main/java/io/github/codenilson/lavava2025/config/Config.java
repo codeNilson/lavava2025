85336d070ba9aee2971086176032078dd5fd4a13
codenilson <fcodenilson@gmail.com>
2025-05-17 17:28:41 -0300

    Refactor player creation logic in Config class; remove commented code for clarity

--- src/main/java/io/github/codenilson/lavava2025/config/Config.java
+++ src/main/java/io/github/codenilson/lavava2025/config/Config.java
@@ -5,6 +5,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import io.github.codenilson.lavava2025.entities.Player;
 import io.github.codenilson.lavava2025.repositories.PlayerRepository;
 
 @Configuration
@@ -16,12 +17,18 @@ public class Config implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        // Player player = new Player();
-        // player.setUserName("Test Player");
-        // player.setPassWord("123456");
-        // player.setActive(true);
+        Player player = new Player();
+        player.setUserName("Test Player");
+        player.setPassWord("123456");
+        player.setActive(true);
+        playerRepository.save(player);
 
-        // playerRepository.save(player);
-        // System.out.println("Player created: " + player.getUserName());
+        Player player2 = new Player();
+        player2.setUserName("Test Player 2");
+        player2.setPassWord("123456");
+        player2.setActive(true);
+        playerRepository.save(player2);
+
+        System.out.println("Player created: " + player.getUserName());
     }
 }
