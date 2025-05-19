acd5b0546f267f153dcdc26b8c33a908605185b3
codenilson <fcodenilson@gmail.com>
2025-05-19 18:25:03 -0300

    Refactor import statements in PlayerController for clarity and organization

--- src/main/java/io/github/codenilson/lavava2025/controllers/PlayerController.java
+++ src/main/java/io/github/codenilson/lavava2025/controllers/PlayerController.java
@@ -1,12 +1,19 @@
 package io.github.codenilson.lavava2025.controllers;
 
-import jakarta.websocket.server.PathParam;
-import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import io.github.codenilson.lavava2025.entities.Player;
 import io.github.codenilson.lavava2025.repositories.PlayerRepository;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("players")
