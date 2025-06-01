93a1f22fd0973547730c12d27de19a6a60cb8226
codenilson <fcodenilson@gmail.com>
2025-06-01 10:23:47 -0300

    initial commit

--- src/main/java/io/github/codenilson/lavava2025/entities/MatchPlayer.java
+++ src/main/java/io/github/codenilson/lavava2025/entities/MatchPlayer.java
@@ -32,6 +32,13 @@ public class MatchPlayer {
     @JoinColumn(name = "match_id")
     private Match match;
 
+    private Integer kills;
+    private Integer deaths;
+    private Integer assists;
+
+    // criar entidade depois
+    private Integer agent;
+
     @CreatedDate
     @Column(nullable = false, updatable = false)
     private LocalDateTime createdAt;
@@ -43,10 +50,14 @@ public class MatchPlayer {
     public MatchPlayer() {
     }
 
-    public MatchPlayer(Player player, Match match) {
+    public MatchPlayer(Player player, Match match, Integer kills, Integer deaths, Integer assists, Integer agent) {
         this.id = new PlayerMatchPk(player.getId(), match.getId());
         this.player = player;
         this.match = match;
+        this.kills = kills;
+        this.deaths = deaths;
+        this.assists = assists;
+        this.agent = agent;
     }
 
     public PlayerMatchPk getId() {
@@ -59,5 +70,89 @@ public class MatchPlayer {
 
     public void setPlayer(Player player) {
         this.player = player;
+
+        if (id == null) {
+            this.id = new PlayerMatchPk();
+        }
+        this.id.setPlayerId(player.getId());
     }
+
+    public Match getMatch() {
+        return match;
+    }
+
+    public void setMatch(Match match) {
+        this.match = match;
+
+        if (id == null) {
+            this.id = new PlayerMatchPk();
+        }
+        this.id.setMatchId(match.getId());
+    }
+
+    public Integer getKills() {
+        return kills;
+    }
+
+    public void setKills(Integer kills) {
+        this.kills = kills;
+    }
+
+    public Integer getDeaths() {
+        return deaths;
+    }
+
+    public void setDeaths(Integer deaths) {
+        this.deaths = deaths;
+    }
+
+    public Integer getAssists() {
+        return assists;
+    }
+
+    public void setAssists(Integer assists) {
+        this.assists = assists;
+    }
+
+    public Integer getAgent() {
+        return agent;
+    }
+
+    public void setAgent(Integer agent) {
+        this.agent = agent;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        MatchPlayer other = (MatchPlayer) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
 }
