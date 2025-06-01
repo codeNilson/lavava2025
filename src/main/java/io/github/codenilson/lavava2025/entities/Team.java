7b8b20431c0caf84f83057fe9f1edc1a3edfb4dc
denilson <fcodenilson@gmail.com>
2025-05-31 21:43:44 -0300

    Add auditing fields to Team entity and update PlayerRepositoryTest with password setting

--- src/main/java/io/github/codenilson/lavava2025/entities/Team.java
+++ src/main/java/io/github/codenilson/lavava2025/entities/Team.java
@@ -1,10 +1,17 @@
 package io.github.codenilson.lavava2025.entities;
 
+import java.time.LocalDateTime;
 import java.util.Set;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -12,6 +19,7 @@ import jakarta.persistence.OneToMany;
 import jakarta.persistence.Transient;
 
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 public class Team {
 
     @Id
@@ -20,6 +28,14 @@ public class Team {
 
     @OneToMany(mappedBy = "id.team")
     private Set<PlayerTeam> players;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
 
     public Team() {
     }
@@ -32,7 +48,5 @@ public class Team {
     public Set<Player> getPlayers() {
         return players.stream().map(PlayerTeam::getPlayer).collect(Collectors.toSet());
     }
-    
 
-    // private String name;
 }
