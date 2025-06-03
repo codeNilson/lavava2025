c78c56eb8a708484e189573be533fb204b6622fe
denilson <fcodenilson@gmail.com>
2025-06-02 22:33:37 -0300

    Enhance PlayerCreateDTO validation: add username length and password complexity constraints

--- src/main/java/io/github/codenilson/lavava2025/dto/player/PlayerCreateDTO.java
+++ src/main/java/io/github/codenilson/lavava2025/dto/player/PlayerCreateDTO.java
@@ -1,13 +1,18 @@
 package io.github.codenilson.lavava2025.dto.player;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 public class PlayerCreateDTO {
 
     @NotBlank(message = "Username is required")
+    @Size(max = 15, min = 4, message = "Username must be between 4 and 15 characters")
     private String username;
 
     @NotBlank(message = "Password is required")
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]+$", message = "Password must have at least one uppercase letter, one lowercase letter, one number, and one special character")
+    @Size(min = 8, max = 20, message = "Password must be between 8 and 20 characters")
     private String password;
 
     private String agent;
