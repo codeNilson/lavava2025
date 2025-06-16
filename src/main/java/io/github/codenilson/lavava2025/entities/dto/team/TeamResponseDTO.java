fbe08941b857d64f62c52da62972499943004de4
codenilson <fcodenilson@gmail.com>
2025-06-16 15:48:58 -0300

    feat: Implement TeamResponseDTO for team data transfer

---
+++ src/main/java/io/github/codenilson/lavava2025/entities/dto/team/TeamResponseDTO.java
@@ -0,0 +1,25 @@
+package io.github.codenilson.lavava2025.entities.dto.team;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+import java.util.UUID;
+
+import org.springframework.beans.BeanUtils;
+
+import io.github.codenilson.lavava2025.entities.Match;
+import io.github.codenilson.lavava2025.entities.PlayerTeam;
+import io.github.codenilson.lavava2025.entities.Team;
+import lombok.Data;
+
+@Data
+public class TeamResponseDTO {
+    private UUID id;
+    private Match match;
+    private Set<PlayerTeam> players;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    private TeamResponseDTO(Team team) {
+        BeanUtils.copyProperties(team, this);
+    }
+}
