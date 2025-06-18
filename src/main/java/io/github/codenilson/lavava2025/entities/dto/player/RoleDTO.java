0c4abc3d4c29784832fc126bf8c0836e6eab6e14
Denilson <fcodenilson@gmail.com>
2025-06-18 11:49:27 -0300

    fix: Update validation annotations in RoleDTO and TeamCreateDTO to use @NotNull instead of @NotBlank

--- src/main/java/io/github/codenilson/lavava2025/entities/dto/player/RoleDTO.java
+++ src/main/java/io/github/codenilson/lavava2025/entities/dto/player/RoleDTO.java
@@ -3,12 +3,12 @@ package io.github.codenilson.lavava2025.entities.dto.player;
 import java.util.Set;
 
 import io.github.codenilson.lavava2025.entities.valueobjects.Roles;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class RoleDTO {
-    @NotBlank(message = "Role cannot be blank")
+    @NotNull(message = "Role cannot be blank")
     private Set<Roles> roles;
 
     public RoleDTO() {
--- src/main/java/io/github/codenilson/lavava2025/entities/dto/team/TeamCreateDTO.java
+++ src/main/java/io/github/codenilson/lavava2025/entities/dto/team/TeamCreateDTO.java
@@ -4,15 +4,15 @@ import java.util.List;
 import java.util.UUID;
 
 import io.github.codenilson.lavava2025.entities.Match;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class TeamCreateDTO {
-    @NotBlank(message = "Match cannot be empty")
+    @NotNull(message = "Match cannot be empty")
     private Match match;
 
-    @NotBlank(message = "You must provide the ID's of the players in the team")
+    @NotNull(message = "You must provide the ID's of the players in the team")
     private List<UUID> players;
 
     public TeamCreateDTO() {
