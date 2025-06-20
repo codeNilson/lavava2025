8162423760d391012c46802b5836eb43284da69a
codenilson <fcodenilson@gmail.com>
2025-06-20 10:02:40 -0300

    test: Enhance TeamServiceTest with match and player setup for team creation

--- src/test/java/io/github/codenilson/lavava2025/unit/services/TeamServiceTest.java
+++ src/test/java/io/github/codenilson/lavava2025/unit/services/TeamServiceTest.java
@@ -5,6 +5,9 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
+import java.util.Set;
+import java.util.UUID;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -12,6 +15,8 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
+import io.github.codenilson.lavava2025.entities.Match;
+import io.github.codenilson.lavava2025.entities.Player;
 import io.github.codenilson.lavava2025.entities.Team;
 import io.github.codenilson.lavava2025.entities.dto.team.TeamCreateDTO;
 import io.github.codenilson.lavava2025.entities.dto.team.TeamResponseDTO;
@@ -43,8 +48,24 @@ public class TeamServiceTest {
     public void saveShouldCreateNewTeam() {
 
         // Given
+        Match match = new Match();
+
+        Set<UUID> playersIds = Set.of(UUID.randomUUID(), UUID.randomUUID());
+        Player player1 = new Player("player1", "password1");
+        Player player2 = new Player("player2", "password2");
+        Set<Player> players = Set.of(player1, player2);
+
         TeamCreateDTO teamCreateDTO = new TeamCreateDTO();
+        teamCreateDTO.setMatch(match);
+        teamCreateDTO.setPlayers(playersIds);
+
         Team team = new Team();
+        team.setMatch(match);
+        team.setPlayers(players);
+
+        TeamResponseDTO teamResponseDTO = new TeamResponseDTO(team);
+
+        when(playerService.findPlayersByIds(playersIds)).thenReturn(players);
         when(teamMapper.toEntity(teamCreateDTO)).thenReturn(team);
         when(teamRepository.save(any(Team.class))).thenReturn(team);
 
@@ -54,6 +75,9 @@ public class TeamServiceTest {
         // Then
         assertNotNull(response);
         assertEquals(team.getId(), response.getId());
+        // assertEquals(match, response.getMatch()); // Uncomment when MatchResponseDTO
+        // is available
+        assertEquals(players.size(), response.getPlayers().size());
         Mockito.verify(teamRepository).save(team);
         Mockito.verify(teamMapper).toEntity(teamCreateDTO);
     }
