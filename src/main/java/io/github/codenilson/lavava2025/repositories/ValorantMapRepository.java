a6f1a02206d1a8fcd008a4b6a7450839920ffade
codenilson <fcodenilson@gmail.com>
2025-06-24 06:58:46 -0300

    feat: Implement ValorantMapRepository interface for database operations

---
+++ src/main/java/io/github/codenilson/lavava2025/repositories/ValorantMapRepository.java
@@ -0,0 +1,13 @@
+package io.github.codenilson.lavava2025.repositories;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import io.github.codenilson.lavava2025.entities.ValorantMap;
+
+@Repository
+public interface ValorantMapRepository extends JpaRepository<ValorantMap, UUID> {
+    
+}
