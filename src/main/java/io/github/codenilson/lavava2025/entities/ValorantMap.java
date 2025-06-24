3d9081b8a94a1b4a4640b17024e99fb1cd1709e7
codenilson <fcodenilson@gmail.com>
2025-06-24 06:57:29 -0300

    feat: Add ValorantMap entity with UUID identifier and name field

---
+++ src/main/java/io/github/codenilson/lavava2025/entities/ValorantMap.java
@@ -0,0 +1,26 @@
+package io.github.codenilson.lavava2025.entities;
+
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "maps")
+@Data
+public class ValorantMap {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    public ValorantMap() {
+    }
+}
